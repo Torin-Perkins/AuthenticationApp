e4a5f2624f9cee2d83a3f6278c65b9c4d19a0af7
Torin-Perkins <tdmp42@gmail.com>
2024-10-19 11:01:45 -0700

    Add project files.

---
+++ AuthenticationApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+namespace AuthenticationApp.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("Filename=./AppUsers.db");
+        }
+    }
+}
